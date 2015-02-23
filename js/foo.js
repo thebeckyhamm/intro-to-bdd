86ad74d7cf06cd41850a403be5096bcd7f746279
Becky Hamm <becky@metricslice.com>
2015-02-23 13:28:24 -0500

    made all tests for Foo green
    * added functions to foo.js to return true
    * fixed a typo(?) in test.foo.js to call
      the instance, not the constructor

--- js/foo.js
+++ js/foo.js
@@ -6,6 +6,22 @@ window.Foo = (function() {
 
   Foo.prototype = {
 
+    plusOne: function(n) {
+        return n + 1;
+    },
+
+    sortArray: function(arr) {
+        return _.sortBy(arr);
+    },
+
+    hasValue: function(arr, value) {
+        return _.contains(arr, value);
+    },
+
+    greet: function(name) {
+        return "Hello, " + name + "!";
+    }
+
   };
 
   return Foo;
