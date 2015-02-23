88037c8736b021684f6264b31184a83a310ac50b
Becky Hamm <becky@metricslice.com>
2015-02-23 14:12:45 -0500

    create exponentOf and reverseString tests

--- test/test.bar.js
+++ test/test.bar.js
@@ -18,7 +18,27 @@ describe("Bar", function() {
     it("takes a number and cubes it", function() {
       expect(bar.cubed(3)).eq(27);
     });
-    
+
   });
+
+  describe("exponentOf", function() {
+
+    it("returns a function", function() {
+      expect( typeof bar.exponentOf ).eq("function");
+    });
+
+  });
+
+  describe("reverseString", function() {
+
+    it("takes a string and reverses it", function() {
+
+      var string = "reverse reverse";
+      expect(bar.reverseString(string)).eq("esrever esrever");
+
+    });
+
+  });
+
 
 });
